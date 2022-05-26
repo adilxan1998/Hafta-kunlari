30e3e01f01931a7c5b8c7957fffd2e98bd9d2f19
adilxan1998 <odilxon.esonaliyev1998@gmail.com>
2022-05-26 20:10:13 +0500

    git init .

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const elForm = document.querySelector(".site-form");
+const elInput = document.querySelector(".form-input");
+const elResult = document.querySelector(".result");
+
+elForm.addEventListener("submit", function (o) {
+  o.preventDefault();
+
+  const inputValue = Number(elInput.value);
+
+  switch(inputValue){
+    case 1:
+      elResult.textContent = "Dushanba"
+      break;
+    case 2:
+      elResult.textContent = "Seshanba"
+      break;
+    case 3:
+      elResult.textContent = "Chorshanba"
+      break;        
+    case 4:
+      elResult.textContent = "Payshanba"
+      break;
+    case 5:
+      elResult.textContent = "Juma"
+      break;  
+    case 6:
+      elResult.textContent = "Shanba"
+      break;  
+    case 7:
+      elResult.textContent = "Yakshanba"
+      break;  
+    default:
+      elResult.textContent = "BUNDAYKUN YO`Q☠🤬🤬🤬☠"  
+  }
+})
+  
